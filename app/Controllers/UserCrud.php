90e65fbce302461b1238ef852234f07d7e7a608c
tanimhasanwp <asfak7074@gmail.com>
2023-04-20 14:42:55 +0600

    first commit

---
+++ app/Controllers/UserCrud.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Controllers;
+use App\Models\UserModel;
+use CodeIgniter\Controller;
+class UserCrud extends Controller
+{
+    // show users list
+    public function index(){
+        $userModel = new UserModel();
+        $data['users'] = $userModel->orderBy('id', 'DESC')->findAll();
+        return view('user_view', $data);
+    }
+    // add user form
+    public function create(){
+        return view('add_user');
+    }
+
+    // insert data
+
+    public function store()
+    {
+        helper(['form']);
+        $rules = [
+            'name'          => 'required|min_length[2]|max_length[50]',
+            'email'         => 'required|min_length[4]|max_length[100]|valid_email|is_unique[users.email]'
+        ];
+
+        if($this->validate($rules)){
+            $userModel = new UserModel();
+            $data = [
+                'name'     => $this->request->getVar('name'),
+                'email'    => $this->request->getVar('email')
+            ];
+            $userModel->insert($data);
+            return $this->response->redirect(site_url('/users-list'));
+        }else{
+            $data['validation'] = $this->validator;
+            echo '<script>alert("Email already exist!")</script>';
+            echo view('add_user', $data);
+        }
+
+    }
+    // show single user
+    public function singleUser($id = null){
+        $userModel = new UserModel();
+        $data['user_obj'] = $userModel->where('id', $id)->first();
+        return view('edit_user', $data);
+    }
+    // update user data
+    public function update(){
+        $userModel = new UserModel();
+        $id = $this->request->getVar('id');
+        $data = [
+            'name' => $this->request->getVar('name'),
+            'email'  => $this->request->getVar('email'),
+        ];
+        $userModel->update($id, $data);
+        return $this->response->redirect(site_url('/users-list'));
+    }
+
+    // delete user
+    public function delete($id = null){
+        $userModel = new UserModel();
+        $data['user'] = $userModel->where('id', $id)->delete($id);
+        return $this->response->redirect(site_url('/users-list'));
+    }
+}
